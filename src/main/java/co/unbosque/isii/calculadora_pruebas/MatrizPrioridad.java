c0614f4d8816952af00a210ad14f6d35eebd7533
msarevalo <msarevalo@github.com>
2017-08-21 18:09:56 -0500

    pruebas

---
+++ src/main/java/co/unbosque/isii/calculadora_pruebas/MatrizPrioridad.java
@@ -0,0 +1,13 @@
+package co.unbosque.isii.calculadora_pruebas;
+
+public class MatrizPrioridad {
+
+
+	public static int matriz() {
+		int[][] matriz= new int[4][4];
+		//pruebas
+		
+		return 0;
+	}
+	
+}
