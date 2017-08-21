6f20f1405ec984387dd448edc484d0454ca23fed
msarevalo <msarevalo@github.com>
2017-08-21 18:30:54 -0500

    Cambios en Matriz de Prioridad

--- src/main/java/co/unbosque/isii/calculadora_pruebas/MatrizPrioridad.java
+++ src/main/java/co/unbosque/isii/calculadora_pruebas/MatrizPrioridad.java
@@ -1,12 +1,29 @@
 package co.unbosque.isii.calculadora_pruebas;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MatrizPrioridad {
 
 
 	public static int matriz() {
-		int[][] matriz= new int[4][4];
-		//pruebas
+
+		HashMap<Character, ArrayList<Integer>> columnas = new HashMap<Character, ArrayList<Integer>>();
 		
+		ArrayList<Integer> suma = new ArrayList<Integer>();
+		ArrayList<Integer> resta = new ArrayList<Integer>();
+		ArrayList<Integer> division = new ArrayList<Integer>();
+		ArrayList<Integer> multiplicacion = new ArrayList<Integer>();
+		
+		suma.add(1); suma.add(1); suma.add(0); suma.add(0);
+		resta.add(0); resta.add(1); resta.add(0); resta.add(0);
+		division.add(1); division.add(1); division.add(0); division.add(1);
+		multiplicacion.add(1); multiplicacion.add(1); multiplicacion.add(1); multiplicacion.add(1);
+		
+		columnas.put('+', suma);
+		columnas.put('-', resta);
+		columnas.put('/', division);
+		columnas.put('*', multiplicacion);
 		return 0;
 	}
 	
