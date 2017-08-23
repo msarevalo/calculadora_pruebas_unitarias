ebf4cd0a7f7e78eb3339daa53720d1463ca245e3
msarevalo <msarevalo@unbosque.edu.co>
2017-08-22 20:56:48 -0500

    Cambio en matriz

--- src/main/java/co/unbosque/isii/calculadora_pruebas/MatrizPrioridad.java
+++ src/main/java/co/unbosque/isii/calculadora_pruebas/MatrizPrioridad.java
@@ -5,13 +5,21 @@ import java.util.HashMap;
 import java.util.Hashtable;
 //import java.util.Map;
 
+import org.testng.internal.collections.Pair;
+
 public class MatrizPrioridad {
 	
 	public static int matriz() {
-		ArrayList<Contenido> matriz = new ArrayList<Contenido>();
-		Contenido cont;
+		/*ArrayList<Contenido> matriz = new ArrayList<Contenido>();
+		Contenido cont;*/
 		
-		matriz.add(cont);
+		HashMap<Pair<Character, Character>, Integer> matriz = new HashMap<Pair<Character,Character>, Integer>();
+		Pair<Character, Character> suma = new Pair<Character, Character>('+', '+');
+		Pair<Character, Character> suma_resta = new Pair<Character, Character>('+', '-');
+		
+		matriz.put(suma, 1);
+		matriz.put(suma_resta, 0);
+		//matriz.add(cont);
 		
 		
 		//prueba
