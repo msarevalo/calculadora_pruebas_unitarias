d352224ef11b4f9c4d97a51ca48da1acc3222d00
msarevalo <msarevalo@unbosque.edu.co>
2017-08-23 18:13:48 -0500

    Matriz de prioridad armada

--- src/main/java/co/unbosque/isii/calculadora_pruebas/MatrizPrioridad.java
+++ src/main/java/co/unbosque/isii/calculadora_pruebas/MatrizPrioridad.java
@@ -34,7 +34,66 @@ public class MatrizPrioridad {
 		return matriz.get('+');
 	}
 	
+	public static Integer matrizSuma(Character obten) {
+		HashMap< Character, Integer> suma = new HashMap<Character, Integer>();
+		
+		suma.put('+', 1);
+		suma.put('-', 1);
+		suma.put('*', 0);
+		suma.put('/', 0);
+		Integer resultado = null;
+				
+		resultado = suma.get(obten);
+		
+		return resultado;		
+	}
+	
+	public static Integer matrizResta(Character obten) {
+		HashMap< Character, Integer> resta = new HashMap<Character, Integer>();
+		
+		resta.put('+', 0);
+		resta.put('-', 1);
+		resta.put('*', 0);
+		resta.put('/', 0);
+		Integer resultado = null;
+				
+		resultado = resta.get(obten);
+		
+		return resultado;		
+	}
+	
+	public static Integer matrizMultiplica(Character obten) {
+		HashMap< Character, Integer> multiplica = new HashMap<Character, Integer>();
+		
+		multiplica.put('+', 1);
+		multiplica.put('-', 1);
+		multiplica.put('*', 1);
+		multiplica.put('/', 1);
+		Integer resultado = null;
+				
+		resultado = multiplica.get(obten);
+		
+		return resultado;		
+	}
+	
+	public static Integer matrizDivide(Character obten) {
+		HashMap< Character, Integer> deivide = new HashMap<Character, Integer>();
+		
+		deivide.put('+', 1);
+		deivide.put('-', 1);
+		deivide.put('*', 0);
+		deivide.put('/', 1);
+		Integer resultado = null;
+				
+		resultado = deivide.get(obten);
+		
+		return resultado;		
+	}
+	
 	public static void main(String[] args) {
-		System.out.println();
+		System.out.println(matrizSuma('/'));
+		System.out.println(matrizResta('-'));
+		System.out.println(matrizMultiplica('+'));
+		System.out.println(matrizDivide('+'));
 	}
 }
